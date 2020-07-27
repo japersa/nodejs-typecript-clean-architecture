60115eec16c5c37cad2abaa192f4ff00e93c4952
Alvaro Javier Perez Salcedo <japersa92@gmail.com>
2020-07-27 01:20:18 -0500

    Adding migrations

---
+++ migrations/20200727061840-add-task.js
@@ -0,0 +1,37 @@
+'use strict'
+
+var dbm
+var type
+var seed
+
+/**
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
+exports.setup = function (options, seedLink) {
+  dbm = options.dbmigrate
+  type = dbm.dataType
+  seed = seedLink
+}
+
+exports.up = function (db) {
+  return db.createTable('tasks', {
+    id: {
+      type: 'int',
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: 'string',
+    description: 'string',
+    created_at: 'datetime',
+    updated_at: 'datetime',
+  })
+}
+
+exports.down = function (db) {
+  return db.dropTable('tasks')
+}
+
+exports._meta = {
+  version: 1,
+}
